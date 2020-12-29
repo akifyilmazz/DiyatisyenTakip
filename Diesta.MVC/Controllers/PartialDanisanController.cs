727e394789d0e82f21659a0ccd7e4f1924641e96
akifyilmazz <mehmetakif.akifmehmet@outlook.com>
2020-12-30 01:04:15 +0300

    fix

---
+++ Diesta.MVC/Controllers/PartialDanisanController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Diesta.MVC.Controllers
+{
+    public class PartialDanisanController : Controller
+    {
+        // GET: PartialDanisan
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult DanisanBilgiTemel()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult DiyetOykusu()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Diesta.MVC/Controllers/PartialModalController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Diesta.MVC.Controllers
+{
+    public class PartialModalController : Controller
+    {
+        // GET: PartialModal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult TemelBilgiModal()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult DiyetOykusuModal()
+        {
+            return PartialView();
+        }
+        [HttpGet]
+        public ActionResult BesinAliskanlikModal()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult GecmisModal()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult GenelAliskanlikModal()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult HareketModal()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult KisiselModal()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult FinansModal()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult NotModal()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult EgzersizModal()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult AntropometriModal()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult MenuModal()
+        {
+            return PartialView();
+        }
+    }
+}
