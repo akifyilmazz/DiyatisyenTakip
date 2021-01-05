f52945c5034973d5a5e772c3815085fed10cd7e7
akifyilmazz <mehmetakif.akifmehmet@outlook.com>
2021-01-06 01:27:37 +0300

    06.01.2021

---
+++ Diesta.MVC/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Diesta.MVC.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProfileList()
+        {
+            return View();
+        }
+    }
+}
